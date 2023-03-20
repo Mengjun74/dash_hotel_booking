8b991a0bcde15b7839ffca57f782df87cf4c8747
Zzz74 <mengjun5@student.ubc.ca>
2023-03-20 13:28:45 -0700

    dash ready

--- dash_hotel_app/data_cleaning.py
+++ dash_hotel_app/data_cleaning.py
@@ -1,7 +1,4 @@
 import pandas as pd
-import country_converter as coco
-import geopandas as gpd
-from dateutil import parser
 from datetime import datetime
 
 # Read the data and clean it
@@ -10,22 +7,14 @@ def data_clean(link = "data/hotel_bookings.csv"):
 
   data = data.dropna(subset=['country'])
 
-  data['country_full_name'] = coco.convert(data['country'], 'ISO3', 'name')
-
-  data.loc[data['country_full_name'].isin(['United States']), 'country_full_name'] = 'United States of America'
-  data.loc[data['country_full_name'].isin(['Czechia']), 'country_full_name'] = 'Czech Republic'
-  data.loc[data['country_full_name'].isin(['Bosnia & Herzegovina']), 'country_full_name'] = 'Bosnia and Herzegovina'
-  data.loc[data['country_full_name'].isin(['Serbia']), 'country_full_name'] = 'Republic of Serbia'
-  data.loc[data['country_full_name'].isin(['North Macedonia']), 'country_full_name'] = 'Macedonia'
+  data.loc[data['country'].isin(['United States']), 'country'] = 'United States of America'
+  data.loc[data['country'].isin(['Czechia']), 'country'] = 'Czech Republic'
+  data.loc[data['country'].isin(['Bosnia & Herzegovina']), 'country'] = 'Bosnia and Herzegovina'
+  data.loc[data['country'].isin(['Serbia']), 'country'] = 'Republic of Serbia'
+  data.loc[data['country'].isin(['North Macedonia']), 'country'] = 'Macedonia'
 
   # create a combined date column for the arrival date
   data['arrival_date'] = data.apply(lambda x: datetime.strptime(f"{x['arrival_date_year']} {x['arrival_date_month']} {x['arrival_date_day_of_month']}", '%Y %B %d'), axis=1)
   return data
 
 
-# Read the map polygon data
-def map_df(link = "data/countries.geojson")
-  countries_json = gpd.read_file(link)
-  country_list = countries_json['name'].tolist()
-  full_country_df = pd.DataFrame({'country_full_name': country_list})
-  return countries_json, full_country_df
--- dash_hotel_app/server.py
+++ dash_hotel_app/server.py
@@ -1,21 +1,96 @@
-import pandas as pd
-import plotly.express as px
-import plotly.graph_objects as go
-import dash_core_components as dcc
-import dash_html_components as html
+import dash
+from dash import dcc
+from dash import html
 from dash.dependencies import Input, Output
-import dash_bootstrap_components as dbc
-from datetime import datetime as dt
-from pathlib import Path
-from dash_hotel_app.data_cleaning import data_clean, map_df
+import plotly.express as px
+import pandas as pd
+from dash_hotel_app.data_cleaning import data_clean
 
 
-data = data_clean()
+df = data_clean()
 
-countries_json, full_country_df = map_df()
 
 # App initialization
-external_stylesheets = [dbc.themes.FLATLY]
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app = dash.Dash(__name__)
+
+app.layout = html.Div([
+    html.H1("Dash Hotel Booking Dashboard"),
+    
+    html.Div([
+        html.Label("Time Range:"),
+        dcc.DatePickerRange(
+            id='date_range',
+            min_date_allowed=df['arrival_date'].min(),
+            max_date_allowed=df['arrival_date'].max(),
+            initial_visible_month=df['arrival_date'].min(),
+            start_date=df['arrival_date'].min(),
+            end_date=df['arrival_date'].max()
+        ),
+
+        html.Label("Hotel Type:"),
+        dcc.Dropdown(
+            id='hotel_type',
+            options=[{'label': i, 'value': i} for i in df['hotel'].unique()],
+            value='All'
+        ),
+
+        html.Label("Country:"),
+        dcc.Dropdown(
+            id='country',
+            options=[{'label': i, 'value': i} for i in df['country'].unique()],
+            value='All'
+        )
+    ], style={'width': '25%', 'display': 'inline-block', 'vertical-align': 'top'}),
+
+    html.Div([
+        dcc.Graph(id='booking_trends'),
+        dcc.Graph(id='cancellation_rates')
+    ], style={'width': '50%', 'display': 'inline-block', 'vertical-align': 'top'}),
+
+    html.Div([
+        dcc.Graph(id='average_booking_value'),
+        dcc.Graph(id='popular_countries')
+    ], style={'width': '50%', 'display': 'inline-block', 'vertical-align': 'top'}),
+
+    html.Div([
+        dcc.Graph(id='booked_room_types'),
+        dcc.Graph(id='peak_booking_periods')
+    ], style={'width': '50%', 'display': 'inline-block', 'vertical-align': 'top'}),
+
+    html.Div([
+        html.Label("Ⓒ All rights reserved 2023 - 💪 💜 Proudly built by UBC MDS program students: Mengjun Chen")
+    ], style={'width': '100%', 'display': 'inline-block', 'vertical-align': 'top'})
+])
+
+@app.callback(
+    [Output('booking_trends', 'figure'),
+     Output('cancellation_rates', 'figure'),
+     Output('average_booking_value', 'figure'),
+     Output('popular_countries', 'figure'),
+     Output('booked_room_types', 'figure'),
+     Output('peak_booking_periods', 'figure')],
+    [Input('date_range', 'start_date'),
+     Input('date_range', 'end_date'),
+     Input('hotel_type', 'value'),
+     Input('country', 'value')])
+def update_graphs(start_date, end_date, hotel_type, country):
+    # Filter the data based on user inputs
+    filtered_df = df[(df['arrival_date'] >= start_date) & (df['arrival_date'] <= end_date)]
+    if hotel_type != 'All':
+        filtered_df = filtered_df[filtered_df['hotel'] == hotel_type]
+
+    if country != 'All':
+        filtered_df = filtered_df[filtered_df['country'] == country]
+
+    # Create the figures for each graph
+    booking_trends_fig = px.histogram(filtered_df, x='arrival_date', y='adults', title='Booking Trends')
+    cancellation_rates_fig = px.bar(filtered_df, x='hotel', y='is_canceled', title='Cancellation Rates', color_discrete_sequence=['blue'])
+    average_booking_value_fig = px.density_heatmap(filtered_df, x='arrival_date_month', y='country', z='adr', title='Average Booking Value')
+    popular_countries_fig = px.choropleth(filtered_df, locations='country', locationmode='ISO-3', color='adults', title='Most Popular Countries')
+    booked_room_types_fig = px.pie(filtered_df, names='reserved_room_type', title='Frequently Booked Room Types')
+    peak_booking_periods_fig = px.bar(filtered_df, x='arrival_date_month', y='adults', title='Peak Booking Periods')
+
+    return booking_trends_fig, cancellation_rates_fig, average_booking_value_fig, popular_countries_fig, booked_room_types_fig, peak_booking_periods_fig
+
 
 
